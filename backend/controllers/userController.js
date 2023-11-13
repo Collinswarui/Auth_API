963ab829678478931333ab908addd6bce01055ad
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-11-13 14:18:48 +0300

    created an error handling middleware to handle errors

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,12 @@
+import asyncHandler from 'express-async-handler'
+
 // @desc Auth user/set token
 //  route POST /api/users/auth
 //  @access public
 
-const authUser = (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Auth User'})
-}
+})
 
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
-
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 const port = process.env.PORT || 5000
 import userRoutes from './routes/userRoutes.js' 
 
@@ -11,5 +11,9 @@ const app = express()
 app.use('/api/users', userRoutes)
 
 app.get('/', (req, res) => res.send('Server is ready'))
+
+app.use(notFound)
+app.use(errorHandler)
+
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
