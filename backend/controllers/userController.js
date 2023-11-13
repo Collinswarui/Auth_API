a7e53c545d79a9cbab491f559cdc329630751a2a
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-11-14 00:24:14 +0300

    added user registration logic in the to allow user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -13,7 +13,32 @@ const authUser = asyncHandler(async (req, res) => {
 // route   POST /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Register user'})
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+
+    if(userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user =  await User.create({
+        name,
+        email,
+        password
+    })
+
+    if (user) {
+        res.status(201).json({
+
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
 })
 
 
--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
 // route for user auth api
 app.use('/api/users', userRoutes)
 
