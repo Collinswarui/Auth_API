cad6bac521dc69e731edb38b0dc54b96f1009608
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-11-13 13:03:43 +0300

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/', (req, res) => res.send('Server is ready'))
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
