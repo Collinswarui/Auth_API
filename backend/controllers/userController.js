b39fa751a2f0f9320167e2d729c777ac68f39d64
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-11-14 01:13:37 +0300

    added user login logic in the user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,21 @@ import User from '../models/userModel.js'
 //  route   POST /api/users/auth
 //  @access Public
 const authUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Auth User'})
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+
+    if (user && (await user.matchPasswords(password))) {
+        generateToken(res, user._id)
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        })
+    } else {
+        res.status(401)
+        throw new Error('Invalid email or password')
+    }
 })
 
 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -20,6 +20,7 @@ const userSchema = mongoose.Schema({
     timestamps: true
 })
 
+// Generate a json web token when a new user is registered in the system
 userSchema.pre('save', async function (next) {
     if(!this.isModified('password')) {
         next()
@@ -28,6 +29,11 @@ userSchema.pre('save', async function (next) {
     const salt = await bcrypt.genSalt(10)
     this.password = await bcrypt.hash(this.password, salt)
 })
+
+// Match entered password with the hashed password in the database
+userSchema.methods.matchPasswords = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
 
 const User =  mongoose.model('User', userSchema)
 
