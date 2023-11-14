62fed18b78a0bba7316eaded106da676b25b15eb
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-11-15 00:18:05 +0300

    Backend complete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -98,7 +98,7 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 
         const updatedUser = await user.save()
 
-        res.status(200).json({
+        res.json({
             _id: updatedUser._id,
             name: updatedUser.name,
             email: updatedUser.email,
