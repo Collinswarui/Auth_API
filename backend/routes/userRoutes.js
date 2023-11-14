92e34eac4d5541ecc0681b89f52181eef7cbf9a5
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-11-14 23:42:41 +0300

    created auth middleware to protect user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,11 +10,14 @@ import {
     getUserProfile,
     updateUserProfile
  } from '../controllers/userController.js'
+ import { protect } from '../middleware/authMiddleware.js'
 
 router.post('/', registerUser)
 router.post('/auth', authUser)
 router.post('/logout', logoutUser)
-router.route('/profile').get(getUserProfile).put(updateUserProfile)
+router.route('/profile')
+   .get(protect, getUserProfile)
+   .put(protect, updateUserProfile)
 
 
 
