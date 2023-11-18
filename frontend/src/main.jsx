ac29d85023ca696b37cee3941440889b8f1d7fb4
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-11-18 14:37:02 +0300

    created login screen and form container

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,11 +10,13 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import HomeScreen from './Screens/HomeScreen.jsx'
+import LoginScreen from './Screens/Loginscreen.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
         <Route index={ true } path='/' element={<HomeScreen />} />
+        <Route path='/login' element={<LoginScreen /> } />
     </Route>
   )
 )
