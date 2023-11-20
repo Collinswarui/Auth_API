cadd7fa33ebef01a130ba6bfc02b56d164c11587
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-11-20 18:18:18 +0300

    added logic to profile update

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -22,10 +22,22 @@ export const usersApiSlice = apiSlice.injectEndpoints({
             query: () => ({
                 url: `${USERS_URL}/logout`,
                 method: 'POST'
-            })
-        })
+            }),
+        }),
+        updateUser: builder.mutation({
+            query: (data) => ({
+                url: `${USERS_URL}/profile`,
+                method: 'PUT',
+                body: data
+            }),
+        }),
     })
 })
 
 
-export const { useLoginMutation, useLogoutMutation, useRegisterMutation } = usersApiSlice
+export const { 
+    useLoginMutation,
+    useLogoutMutation,
+    useRegisterMutation,
+    useUpdateUserMutation
+    } = usersApiSlice
