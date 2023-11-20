61a5508bdb23455d11bf27e29d100924ae0d2278
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-11-20 17:33:49 +0300

    added a protection to the private routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,9 +11,11 @@ import { Provider } from 'react-redux'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
+import PrivateRoute from './components/PrivateRoute.jsx'
 import HomeScreen from './Screens/HomeScreen.jsx'
 import LoginScreen from './Screens/Loginscreen.jsx'
 import RegisterScreen from './Screens/RegisterScreen.jsx'
+import Profile from './Screens/Profile.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -22,6 +24,10 @@ const router = createBrowserRouter(
           <Route index={ true } path='/' element={<HomeScreen />} />
           <Route path='/login' element={<LoginScreen /> } />
           <Route path='/register' element={<RegisterScreen /> } />
+          {/* Private routes */}
+          <Route path='' element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />} />
+          </Route>
 
       </Route>
   )
