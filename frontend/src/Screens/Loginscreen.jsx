975e163aa4ec360df1d0c1c71a159415f8e4a939
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-11-20 15:56:06 +0300

    added a loader on the login page

--- frontend/src/Screens/Loginscreen.jsx
+++ frontend/src/Screens/Loginscreen.jsx
@@ -6,6 +6,7 @@ import FormContainer from '../components/FormContainer'
 import { useLoginMutation } from "../slices/usersApiSlice"
 import { setCredentials } from "../slices/authSlice"
 import { toast } from "react-toastify"
+import Loader from "../components/Loader"
 
 
 const LoginScreen = () => {
@@ -59,6 +60,8 @@ const LoginScreen = () => {
                     onChange={ (e) => setPassword(e.target.value) }
                 ></Form.Control>
               </Form.Group>
+
+              {isLoading && <Loader />}
               <Button type="submit" variant="primary" className="mt-3">
                 Sign In
               </Button>
